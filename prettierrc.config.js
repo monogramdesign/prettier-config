7be99dfe4e2e0d32fa46e707f9679cba1d345508
Julien Martel <julien@monogram.io>
2023-08-18 10:14:53 -0500

    config changes

--- prettierrc.config.js
+++ prettierrc.config.js
@@ -1,10 +1,8 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  tabWidth: 2,
   printWidth: 100,
   singleQuote: true,
   trailingComma: "es5",
-  bracketSameLine: false,
   semi: false,
   plugins: ["prettier-plugin-tailwindcss"],
 };
