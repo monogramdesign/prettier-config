4cad50d04bb21db150d4db0ad28366795452e6c4
Julien Martel <julien@monogram.io>
2023-08-18 10:03:04 -0500

    type recognition for config and change trailing comma

--- prettierrc.config.js
+++ prettierrc.config.js
@@ -1,8 +1,9 @@
+/** @type {import('prettier').Config} */
 module.exports = {
   tabWidth: 2,
   printWidth: 100,
   singleQuote: true,
-  trailingComma: "none",
+  trailingComma: "es5",
   bracketSameLine: false,
   semi: false,
   plugins: ["prettier-plugin-tailwindcss"],
