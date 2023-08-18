29bd9f10e6cd139ba965f324411adee853bd7412
Julien Martel <julien@monogram.io>
2023-08-18 10:54:26 -0500

    lint all files with self-consuming config, create lint step in workflow

--- npmjs.md
+++ npmjs.md
@@ -1,5 +1,7 @@
 ### Push to npmjs.com
+
 ```sh
 npm publish --access public
 ```
+
 Read more on npmjs.com [Publishing scoped public packages](https://docs.npmjs.com/creating-and-publishing-scoped-public-packages).
--- src/constants.ts
+++ src/constants.ts
@@ -1,21 +1,21 @@
-import type { PackageManagerRecord } from "./types";
+import type { PackageManagerRecord } from './types'
 
-export const PACKAGE_NAME = "@monogram/prettier-config";
+export const PACKAGE_NAME = '@monogram/prettier-config'
 
-export const ESLINT_FILENAME = ".eslintrc.js";
+export const ESLINT_FILENAME = '.eslintrc.js'
 
-export const AVAILABLE_CONFIGS = ["next", "node"] as const;
+export const AVAILABLE_CONFIGS = ['next', 'node'] as const
 
-export const PACKAGE_MANAGERS = ["yarn", "pnpm", "npm"] as const;
+export const PACKAGE_MANAGERS = ['yarn', 'pnpm', 'npm'] as const
 
 export const LOCK_FILES: PackageManagerRecord = {
   yarn: `${process.cwd()}/yarn.lock`,
   pnpm: `${process.cwd()}/pnpm-lock.yaml`,
   npm: `${process.cwd()}/package-lock.json`,
-} as const;
+} as const
 
 export const INSTALL_PREFIXES: PackageManagerRecord = {
-  yarn: "yarn add -D",
-  pnpm: "pnpm add -D",
-  npm: "npm i -D",
-} as const;
+  yarn: 'yarn add -D',
+  pnpm: 'pnpm add -D',
+  npm: 'npm i -D',
+} as const
