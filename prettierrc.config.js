ceb6f73fa5a1aaab174fd903e2ec256e2331efdc
Claudio F <claudio@monogram.io>
2023-09-18 10:32:54 -0400

    added tabs in the config

--- prettierrc.config.js
+++ prettierrc.config.js
@@ -1,8 +1,11 @@
 /** @type {import('prettier').Config} */
 module.exports = {
+  tabWidth: 2,
+  useTabs: true,
   printWidth: 100,
   singleQuote: true,
   trailingComma: 'es5',
+  bracketSameLine: false,
   semi: false,
   plugins: ['prettier-plugin-tailwindcss'],
 }
